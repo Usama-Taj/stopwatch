235ea0436b8008c1a3a20e29779986884d547de9
Usama-Taj <usama.taj@hotmail.com>
2022-07-07 11:02:50 +0500

    1. Added Reset Functionality for Timer. 2. Added Enable/Disable Buttons based on the condition of the timer. 3. Recording Recent Actions of the timer.

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ This application is a basic timer with start, pause, split and reset features
 ### COMMITS (chronological)
 
 - Added Pause and Split Functionality.
+- Added README.md File
+- Added Reset Functionality
 
 ### LANGUAGES USED
 
--- js/eventListeners.js
+++ js/eventListeners.js
@@ -3,20 +3,25 @@ let MINUTES = 0;
 let SECONDS = 0;
 let counter = 0;
 let recentAction = "";
+let recentActionTimer = "";
 let INTERVAL;
 
-const tableData = [];
+let tableData = [];
 
 const startButton = document.querySelector(".start");
 const pauseButton = document.querySelector(".pause");
 const splitButton = document.querySelector(".split");
 const resetButton = document.querySelector(".reset");
-const tableContent = document.querySelector(".timer-table");
+const tableContent = document.querySelector(".table-body");
+const recentTimer = document.querySelector(".timer-sub");
 
 splitButton.addEventListener("click", splitClicked);
 resetButton.addEventListener("click", resetClicked);
 pauseButton.addEventListener("click", pauseClicked);
 startButton.addEventListener("click", startClicked);
+
+disableButton(resetButton);
+disableButton(splitButton);
 
 /* **************** */
 /* Render Functions */
@@ -29,7 +34,9 @@ function showCounter(counter) {
   } else return "00";
 }
 function renderTimer() {
-  ++SECONDS;
+  if (recentAction !== "Reset") {
+    ++SECONDS;
+  }
   if (SECONDS > 60) {
     SECONDS = 0;
     ++MINUTES;
@@ -78,38 +85,94 @@ function renderTable(action) {
   <td>${recentAction}</td>`;
   tableContent.appendChild(tableRow);
 }
+function disableButton(button) {
+  button.classList.add("disabled");
+  button.disabled = true;
+}
+function enableButton(button) {
+  button.classList.remove("disabled");
+  button.disabled = false;
+}
+function setRecentTimer(hours, minutes, seconds) {
+  recentTimer.innerHTML = `<div id="hours">${showCounter(hours)}</div>
+  <div class="colon">:</div>
+  <div id="minutes">${showCounter(minutes)}</div>
+  <div class="colon">:</div>
+  <div id="seconds">${showCounter(seconds)}</div>
+  <div class="dot">.</div>
+  <div class="fraction">6</div>
+  <div class="fraction-fraction"><sup>26</sup></div>`;
+}
 /* *************** */
 /* Event Functions */
 /* *************** */
 function startClicked(e) {
   INTERVAL = setInterval(renderTimer, 1000);
+  enableButton(splitButton);
+  enableButton(resetButton);
   console.log(e.target.innerText);
+  enableButton(splitButton);
+  recentAction = e.target.innerText;
   renderPauseButton();
 }
+
 function pauseClicked(e) {
+  // Increment Table Row Offset
   ++counter;
+  // Record Recent Action
   recentAction = e.target.innerText;
+  // Clear Timer
   clearInterval(INTERVAL);
   console.log(e.target.innerText);
+  // Insert Row to table for pause action
   renderTable(recentAction);
   tableData.push(
     counter,
     { hours: HOURS, minutes: MINUTES, seconds: SECONDS },
     recentAction
   );
+  // Disable Split Button once in paused state
+  disableButton(splitButton);
+  // Render Start Button to resume timer
   renderStartButton();
+  // Set Recent Timer in timer-sub class
+  setRecentTimer(HOURS, MINUTES, SECONDS);
 }
 function splitClicked(e) {
+  // Increment the Table Row Offer
   ++counter;
+  // Record recent performed Action for Table
   recentAction = e.target.innerText;
   console.log(e.target.innerText);
+  // Rerender Table with new row for Split Action
   renderTable(recentAction);
   tableData.push(
     counter,
     { hours: HOURS, minutes: MINUTES, seconds: SECONDS },
     recentAction
   );
+  // Set Recent Timer in timer-sub class
+  setRecentTimer(HOURS, MINUTES, SECONDS);
 }
 function resetClicked(e) {
+  // Stop Timer
+  clearInterval(INTERVAL);
+  MINUTES = 0;
+  HOURS = 0;
+  SECONDS = 0;
+  // Re render Timer with 00:00:00
+  recentAction = e.target.innerText;
+  renderTimer();
+  // Display Start Button instead of Pause Button
+  renderStartButton();
+  // Enable Split Button if Timer is Paused;
+  enableButton(splitButton);
+  // Empty Table Content
+  tableData = [];
+  tableContent.innerHTML = "";
   console.log(e.target.innerText);
+  // Disable Reset and Split Button
+  disableButton(resetButton);
+  disableButton(splitButton);
+  renderStartButton();
 }
